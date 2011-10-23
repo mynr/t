46e7bcc0f233a7812ab3140a341667dd2c0900fa
t <no@mail.com>
2011-10-23 10:33:38 -0500

    Initial commit of my code

---
+++ code_to_share.rb
@@ -0,0 +1,7 @@
+my_variable = "I have some code I'd like to share"
+
+my_var2 = "and I'd like for others to be able to use my code"
+
+my_var3 = "and I'd also like for them to be able to CONTRIBUTE to my code!"
+
+
